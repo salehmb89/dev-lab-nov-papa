ff08cc5128151cc8a0fa3797ccc1a5641d9a7e02
Saleh Basalim <156353463+salehmb89@users.noreply.github.com>
2024-02-13 16:49:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = (process.env.PORT || 3000)
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://sbasalim:<password>@cluster0.h1oabai.mongodb.net/?retryWrites=true&w=majority";
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -46,3 +48,27 @@ app.get('/index', function (req, res) {
 // app.listen(3000)
 
 app.listen(port, () => console.log(`Server is running...on ${ port }` ));
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env,uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
